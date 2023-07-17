14d80ef667d7b7ded4d9ae8e722e405f65a5480c
menesdurak <m.enes.durak@gmail.com>
2023-07-17 10:55:19 +0300

    Created bottom navigation menu

--- app/src/main/java/com/menesdurak/e_ticaret_uygulamasi/presentation/MainActivity.kt
+++ app/src/main/java/com/menesdurak/e_ticaret_uygulamasi/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.menesdurak.e_ticaret_uygulamasi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.menesdurak.e_ticaret_uygulamasi.R
 import com.menesdurak.e_ticaret_uygulamasi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +16,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavMenu.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> navController.navigate(R.id.homeFragment)
+                R.id.favorites -> navController.navigate(R.id.favoritesFragment)
+                R.id.cart -> navController.navigate(R.id.cartFragment)
+            }
+            true
+        }
     }
 }
