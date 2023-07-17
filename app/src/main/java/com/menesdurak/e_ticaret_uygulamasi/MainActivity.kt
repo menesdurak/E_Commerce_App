0c2d395bb2218a34442114efc6ec29226424aff7
menesdurak <m.enes.durak@gmail.com>
2023-07-17 10:15:03 +0300

    Created navigation graph

--- app/src/main/java/com/menesdurak/e_ticaret_uygulamasi/MainActivity.kt
+++ app/src/main/java/com/menesdurak/e_ticaret_uygulamasi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.menesdurak.e_ticaret_uygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.menesdurak.e_ticaret_uygulamasi.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
