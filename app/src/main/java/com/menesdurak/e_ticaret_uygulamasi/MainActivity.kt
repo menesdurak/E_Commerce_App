f09d2b3378422f933064f968a42db460b898b312
menesdurak <m.enes.durak@gmail.com>
2023-07-17 10:07:57 +0300

    First commit

---
+++ app/src/main/java/com/menesdurak/e_ticaret_uygulamasi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.menesdurak.e_ticaret_uygulamasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
